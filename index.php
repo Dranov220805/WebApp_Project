f467af1bbeca8fb109645baa8c958fcbf9bdabc2
Dranov220805 <thanhlongduono6a3@gmail.com>
2025-01-06 16:44:37 +0700

    update htaccess & gitignore

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
 
+
+    include "./config/Database.php";
+    include "./app/models/index.php";
+    include "./app/repository/index.php";
+    include "./app/services/index.php";
+    include "./route/index.php";
 ?>
