299761a8cffc24a0a2d7622fbe293b3ef5680adc
Dranov220805 <thanhlongduono6a3@gmail.com>
2025-01-06 23:22:14 +0700

    update deploy docker

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
+//
+//
+//    include "./config/Database.php";
+//    include "./app/models/index.php";
+//    include "./app/repository/index.php";
+//    include "./app/services/index.php";
+//    include "./route/index.php";
+//?>
 
-
-    include "./config/Database.php";
-    include "./app/models/index.php";
-    include "./app/repository/index.php";
-    include "./app/services/index.php";
-    include "./route/index.php";
+<?php
+echo"Helloworld";
 ?>
